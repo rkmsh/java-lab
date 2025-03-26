9447d1d0195eb82007d92d86ef4b2ad332140293
rkmsh <rohitshmahato@gmail.com>
2025-03-26 14:08:51 -0400

    home page added

--- soc/src/main/java/com/server/soc/controller/MainController.java
+++ soc/src/main/java/com/server/soc/controller/MainController.java
@@ -3,7 +3,12 @@ package com.server.soc.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 
 @Controller
 public class MainController {
@@ -12,5 +17,17 @@ public class MainController {
     public ResponseEntity<?> test() {
         return new ResponseEntity<>(new String("Working!!!"), HttpStatus.ACCEPTED);
     }
+
+    @GetMapping("/")
+    public String getMethodName(HttpServletRequest request, Model model) {
+        return "home";
+    }
+
+    @GetMapping("/login")
+    public String login(HttpServletRequest request, Model model) {
+        return "login";
+    }
+    
+    
 
 }
